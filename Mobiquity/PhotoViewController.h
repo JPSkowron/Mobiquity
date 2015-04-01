934aef2f29668c8cfa7d81be1560d773584e59e6
JPSkowron <jpskowron@gmail.com>
2015-04-01 16:03:35 -0500

    working on camera features

---
+++ Mobiquity/PhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.h
+//  Mobiquity
+//
+//  Created by JP Skowron on 4/1/15.
+//  Copyright (c) 2015 JP Skowron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController <UIImagePickerControllerDelegate, UIDocumentInteractionControllerDelegate, UINavigationControllerDelegate>
+
+@end
---
+++ Mobiquity/PhotoViewController.m
@@ -0,0 +1,54 @@
+//
+//  PhotoViewController.m
+//  Mobiquity
+//
+//  Created by JP Skowron on 4/1/15.
+//  Copyright (c) 2015 JP Skowron. All rights reserved.
+//
+
+#import "PhotoViewController.h"
+
+@interface PhotoViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation PhotoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onCameraButtonTapped:(UIButton *)sender {
+
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    imagePickerController.editing = YES;
+    imagePickerController.delegate = (id)self;
+
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+
+
+
+}
+
+- (IBAction)onCameraRollButtonTapped:(UIButton *)sender {
+
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    [picker setSourceType:(UIImagePickerControllerSourceTypePhotoLibrary)];
+    [self presentViewController:picker animated:YES completion:nil];
+
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *image = [info valueForKey: UIImagePickerControllerOriginalImage];
+    self.addPhotoImageView.image = image;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end
