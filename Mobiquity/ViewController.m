1b69f64eb096ea20c562e23a5a7ecc989b45f66b
JPSkowron <jpskowron@gmail.com>
2015-03-31 22:58:24 -0500

    dropbox login screen

--- Mobiquity/ViewController.m
+++ Mobiquity/ViewController.m
@@ -17,6 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if (![[DBSession sharedSession] isLinked]) {
+        [[DBSession sharedSession] linkFromController:self];
+    }
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
